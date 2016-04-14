757f797e11383faf7e0aef0271591f81c139f73e
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2016-04-14 15:26:57 -0400

    modifies welcome mailers for students

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,7 +1,20 @@
 class WelcomeMailer < ActionMailer::Base
-
-  def welcome_email(email)
-    @email = email
-    mail(from: 'scholar@uc.edu', to: @email, subject: 'Welcome to Scholar@UC!')
-  end
+	default from: 'scholar@uc.edu'
+	def welcome_email(email)
+		@email = email
+		new_user = User.find_by_email(@email)
+		if new_user.student?
+			mail(
+				to: @email,
+				subject: 'Welcome to Scholar@UC, UC students!',
+				template_name: 'welcome_email_student.html.erb',
+				)
+		else
+			mail(
+				to: @email,
+				subject: 'Welcome to Scholar@UC!',
+				template_name: 'welcome_email.html.erb',
+				)
+		end
+	end
 end
--- spec/features/welcome_mailer_spec.rb
+++ spec/features/welcome_mailer_spec.rb
@@ -17,10 +17,22 @@ describe WelcomeMailer do
 	end
 	after :each do
 		ActionMailer::Base.deliveries = []
+		User.find_by_email(user_email).delete
 	end
-	it 'should send welcome email to user' do
+	it 'should send welcome email to user upon registration' do
 		email.to.should == [user_email]
 		email.from.should == ["scholar@uc.edu"]
 	end
-
+	describe 'Shibboleth Logins' do
+		let(:student_user) { FactoryGirl.create(:user, ucstatus: 'Student')}
+		let(:student_email) { WelcomeMailer.welcome_email(student_user.email)}
+		let(:faculty_user) { FactoryGirl.create(:user, ucstatus: 'AnythingElse')}
+		let(:faculty_email) { WelcomeMailer.welcome_email(faculty_user.email)}
+		it 'should should send a different email to students' do
+			student_email.subject.should == "Welcome to Scholar@UC, UC students!"
+		end
+		it 'should send the default email to non-student users' do
+			faculty_email.subject.should == "Welcome to Scholar@UC!"
+		end
+	end
 end
