02f7c240b1d6dd7dca7cb7535caf90409060d68f
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2016-04-19 09:09:49 -0400

    fixes bug with bad registrations

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -3,18 +3,20 @@ class WelcomeMailer < ActionMailer::Base
 	def welcome_email(email)
 		@email = email
 		new_user = User.find_by_email(@email)
-		if new_user.student?
-			mail(
-				to: @email,
-				subject: 'Welcome to Scholar@UC, UC students!',
-				template_name: 'welcome_email_student.html.erb',
-				)
-		else
-			mail(
-				to: @email,
-				subject: 'Welcome to Scholar@UC!',
-				template_name: 'welcome_email.html.erb',
-				)
+		unless new_user.nil?
+			if new_user.student?
+				mail(
+					to: @email,
+					subject: 'Welcome to Scholar@UC, UC students!',
+					template_name: 'welcome_email_student.html.erb',
+					)
+			else
+				mail(
+					to: @email,
+					subject: 'Welcome to Scholar@UC!',
+					template_name: 'welcome_email.html.erb',
+					)
+			end
 		end
 	end
 end
