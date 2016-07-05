c81b93eb98e4f4c4c7af04a72cff32f44d089eac
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2016-07-05 16:41:39 -0400

    add date_created and notes as searchable fields in curate

--- app/repository_datastreams/etd_datastream.rb
+++ app/repository_datastreams/etd_datastream.rb
@@ -63,7 +63,9 @@ class EtdDatastream < ActiveFedora::NtriplesRDFDatastream
       index.as :searchable, :facetable
     end
 
-    map.note({to: 'description#note', in: RDF::QualifiedDC})
+    map.note({to: 'description#note', in: RDF::QualifiedDC}) do |index|
+      index.as :stored_searchable
+    end
 
     map.publisher({in: RDF::DC}) do |index|
       index.as :stored_searchable, :facetable
